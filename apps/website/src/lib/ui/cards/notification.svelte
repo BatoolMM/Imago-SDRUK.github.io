2ee047658b0d804cbfd7597dc73cb47fa0a35152
artgp <git@arturoguzmanperez.com>
2025-10-03 16:07:44 +0100

    fix: update notification component

--- apps/website/src/lib/ui/cards/notification.svelte
+++ apps/website/src/lib/ui/cards/notification.svelte
@@ -1,38 +1,76 @@
 <script lang="ts">
-	import { colour, notify, textColour } from '$lib/stores/notify'
-	import { fly } from 'svelte/transition'
+	import { notify } from '$lib/stores/notify.js'
+	import { jstr } from '@arturoguzman/art-ui'
+	import { slide } from 'svelte/transition'
 </script>
 
-{#if $notify !== ''}
-	<div
-		id="notification"
-		class="notification-container fixed right-0 top-2 px-3"
-		in:fly|global={{ y: -200, duration: 1000 }}
-		out:fly|global={{ y: -200, duration: 2000 }}
-		on:pointerdown={() => {
-			$notify = ''
-		}}
-		style:--background={$colour}
-		style:--colour={$textColour}
-	>
-		<p
-			class="notification-text flex h-full cursor-pointer items-center justify-center p-4 text-sm md:text-lg"
+<div class="notifications">
+	{#each $notify as notification (notification)}
+		<button
+			onclick={() => {
+				clearTimeout(notification.cancel)
+				notify.remove(notification.id)
+			}}
+			transition:slide
+			class="notification"
+			style:--duration={`${notification.duration}ms`}
 		>
-			{$notify}
-		</p>
-	</div>
-{/if}
+			<p>{notification.status ? `${notification.status}: ` : ''}{notification.message}</p>
+		</button>
+	{/each}
+</div>
 
 <style>
-	.notification-text {
-		border: 1px solid var(--theme-colour-background);
-		color: var(--theme-colour-background);
-		background-color: var(--theme-colour-text);
+	.notification {
+		border: 1px solid var(--theme-colour-highlight);
+		color: var(--theme-colour-text);
+		background-color: var(--theme-colour-background);
+		padding: 1rem;
+		border-radius: 0.35rem;
+		cursor: pointer;
+		/* box-shadow: 2px 2px var(--theme-colour-text); */
+		position: relative;
+		overflow: hidden;
 		font-family: var(--theme-font-title);
-		font-weight: 600;
-		border-radius: 1rem;
+		flex-shrink: 0;
 	}
-	#notification {
-		z-index: 100000;
+	.notification::after {
+		content: '';
+		position: absolute;
+		bottom: 0rem;
+		left: 0rem;
+		width: 0%;
+		height: 0.35rem;
+		background-color: var(--theme-colour-highlight);
+		animation: progress var(--duration) forwards linear;
+		flex-shrink: 0;
+	}
+
+	@keyframes progress {
+		0% {
+			width: 0%;
+		}
+		100% {
+			width: 100%;
+		}
+	}
+
+	.notifications {
+		position: fixed;
+		top: 4rem;
+		right: 1rem;
+		display: flex;
+		flex-direction: column;
+		gap: 0.5rem;
+		z-index: 1000;
+	}
+	@media (min-width: 768px) {
+		.notifications {
+			top: 2rem;
+			right: 2rem;
+			height: calc(100lvh - 4rem);
+			overflow-y: scroll;
+			scrollbar-width: none;
+		}
 	}
 </style>
