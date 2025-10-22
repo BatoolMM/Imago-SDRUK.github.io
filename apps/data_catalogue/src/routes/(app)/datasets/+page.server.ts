9e99c34ee3fa808dc85432410995dee81899fb7f
artgp <git@arturoguzmanperez.com>
2025-10-22 11:57:40 +0100

    feat: add pagination on dataset

--- apps/data_catalogue/src/routes/(app)/datasets/+page.server.ts
+++ apps/data_catalogue/src/routes/(app)/datasets/+page.server.ts
@@ -1,14 +1,29 @@
 import { get } from '$lib/utils/ckan/ckan.js'
+import { jstr } from '@arturoguzman/art-ui'
 import type { PageServerLoadEvent } from './$types.js'
-export const load = async ({ locals }: PageServerLoadEvent) => {
-	const data = await locals.ckan.request(get('current_package_list_with_resources'))
+export const load = async ({ locals, url }: PageServerLoadEvent) => {
+	const offset = url.searchParams.get('offset') ? Number(url.searchParams.get('offset')) : 0
+	const limit = url.searchParams.get('limit') ? Number(url.searchParams.get('limit')) : 10
+	if (offset === 0) {
+		url.searchParams.delete('offset')
+	}
+	const data = await locals.ckan.request(
+		get('current_package_list_with_resources', { offset: offset <= 0 ? 0 : offset, limit })
+	)
+	if (data.success === false) {
+		console.log(jstr(data.result))
+	}
+	const resources = [
+		...new Set(data.result.flatMap(({ resources }) => resources).map((r) => r.format))
+	]
 	return {
 		data,
 		datasets: await locals.ckan.request(get('package_list')),
+		datasets_count: (await locals.ckan.request(get('package_list'))).result.length,
 		groups: await locals.ckan.request(get('group_list')),
 		organisations: await locals.ckan.request(get('organization_list')),
 		tags: await locals.ckan.request(get('tag_list')),
-		resources: await locals.ckan.request(get('format_autocomplete')),
+		resources: { result: resources },
 		licenses: await locals.ckan.request(get('license_list'))
 	}
 }
--- apps/data_catalogue/src/routes/(app)/datasets/+page.svelte
+++ apps/data_catalogue/src/routes/(app)/datasets/+page.svelte
@@ -1,131 +1,104 @@
 <script lang="ts">
 	import { debug } from '$lib/globals/dev.svelte.js'
 	import { jstr } from '@arturoguzman/art-ui'
-	import {
-		Accordion,
-		BaseCard,
-		BaseSection,
-		Button,
-		Fact,
-		Icon,
-		Paragraph,
-		Subtitle
-	} from '@imago/ui'
+	import { BaseCard, BaseSection, Button, Icon, Paragraph, Subtitle } from '@imago/ui'
+	import Notes from '$lib/ui/text/notes.svelte'
+	import { page } from '$app/state'
+	import Filters from '$lib/ui/dataset/filters.svelte'
 
 	let { data } = $props()
 	let datasets = $derived(data.data.result)
-	const filters = [
-		{
-			title: 'Organisations',
-			filters: data.organisations,
-			transform: (id: string) => {
-				return { label: id, href: `?organization=${id}` }
-			}
-		},
-		{
-			title: 'Groups',
-			filters: data.groups,
-			transform: (id: string) => {
-				return { label: id, href: `?groups=${id}` }
-			}
-		},
-		{
-			title: 'Tags',
-			filters: data.tags,
-			transform: (id: string) => {
-				return { label: id, href: `?tags=${id}` }
-			}
-		},
-		{
-			title: 'Resources',
-			filters: data.resources,
-			transform: (id: string) => {
-				return { label: id, href: `?res_format=${id}` }
-			}
-		},
-		{
-			title: 'Licenses',
-			filters: data.licenses,
-			transform: (id: string) => {
-				return { label: id, href: `?license_id=${id}` }
-			}
-		}
-	]
 </script>
 
 <BaseSection>
 	<div class="datasets-section">
 		<div class="left-col">
-			{#each filters as filter}
-				<Accordion default_open>
-					{#snippet title({ toggleOpen, open })}
-						<Button
-							style="clean-full"
-							onclick={() => {
-								toggleOpen()
-							}}
-						>
-							{#snippet leftCol()}
-								<Subtitle>{filter.title}</Subtitle>
-							{/snippet}
-							{#snippet rightCol()}
-								<Icon icon={{ icon: open ? 'arrow-down-01' : 'arrow-right-01', set: 'hugeicons' }}
-								></Icon>
-							{/snippet}
-						</Button>
-					{/snippet}
-					{#if Array.isArray(filter.filters.result)}
-						<div class="accordion-buttons">
-							{#each filter.filters.result as li}
-								<Button>{li}</Button>
-							{/each}
-						</div>
-					{/if}
-				</Accordion>
-			{/each}
-
-			{#if Array.isArray(datasets)}
-				{#each datasets as dataset}
-					<Button href="/datasets/{dataset}">{dataset}</Button>
-				{/each}
-			{/if}
+			<Filters
+				organisations={data.organisations}
+				tags={data.tags}
+				licenses={data.licenses}
+				groups={data.groups}
+				resources={data.resources}
+			></Filters>
 		</div>
 		<div class="right-col">
 			{#if Array.isArray(datasets)}
 				{#each datasets as dataset}
 					<BaseCard>
-						<a href={`/datasets/${dataset.id}`} class="product-card">
-							<div class="title">
-								<Subtitle size="md">{dataset.title}</Subtitle>
-							</div>
+						<div class="product-card">
+							<a href={`/datasets/${dataset.name}`} class="title">
+								<Subtitle weight={600} current_colour size="md">{dataset.title}</Subtitle>
+							</a>
 							<div class="metadata">
 								<div class="notes">
 									{#if dataset.notes}
-										<Paragraph>{dataset.notes}</Paragraph>
+										<Notes note={String(dataset.notes)} trim={200}></Notes>
 									{/if}
 								</div>
-								<div class="facts">
-									{#if dataset.license_title}
-										<Fact title="License" text={String(dataset.license_title)}></Fact>
-									{/if}
-									{#if dataset.resources && Array.isArray(dataset.resources)}
-										{@const formats = Array.from(
-											new Set(dataset.resources.map((resource) => resource.format))
-										)}
-										<Fact title="Format" text={String(formats.join(', '))}></Fact>
-									{/if}
+								<div class="footer">
+									<div class="facts">
+										{#if dataset.resources && Array.isArray(dataset.resources)}
+											{@const formats = Array.from(
+												new Set(dataset.resources.map((resource) => resource.format))
+											)}
+
+											<Paragraph size="xs"
+												>{formats.length === 1 ? 'Format' : 'Formats'}: {String(
+													formats.join(', ')
+												)}</Paragraph
+											>
+										{/if}
+										{#if dataset.license_title}
+											<Paragraph size="xs">License: {String(dataset.license_title)}</Paragraph>
+										{/if}
+									</div>
+									<Button href={`/datasets/${dataset.name}`}>
+										See more
+										<Icon icon={{ icon: 'arrow-narrow-right', set: 'tabler' }}></Icon>
+									</Button>
 								</div>
 							</div>
-						</a>
+						</div>
 					</BaseCard>
 				{/each}
 			{/if}
+			<div class="footer">
+				{#if page.url.searchParams.get('offset')}
+					{@const offset = Number(page.url.searchParams.get('offset'))}
+					<!-- HACK: replace fixed dataset with fn to strip required searchparams -->
+					<Button href={offset - 10 <= 0 ? '/datasets' : `?offset=${offset - 10}`}>
+						<Icon icon={{ icon: 'arrow-narrow-left', set: 'tabler' }}></Icon>
+					</Button>
+					<div class="page-count">
+						<Paragraph>
+							Page {(offset / 10 + 1).toFixed(0)} of
+							{(Number(data.datasets_count) / 10).toFixed(0)}
+						</Paragraph>
+					</div>
+					<Button href={`?offset=${offset + 10}`}>
+						<Icon icon={{ icon: 'arrow-narrow-right', set: 'tabler' }}></Icon>
+					</Button>
+				{:else}
+					<div class="no-button"></div>
+					<div class="page-count">
+						<Paragraph>
+							Page 1 of
+							{(Number(data.datasets_count) / 10).toFixed(0)}
+						</Paragraph>
+					</div>
+					<div class="button-wrapper">
+						<Button href={`?offset=10`}>
+							<Icon icon={{ icon: 'arrow-narrow-right', set: 'tabler' }}></Icon>
+						</Button>
+					</div>
+				{/if}
+			</div>
 		</div>
 	</div>
 </BaseSection>
 
 {#if debug.status}
-	<pre>{jstr(data)}</pre>
+	<pre>{jstr(data.licenses)}</pre>
 {/if}
 
 <style>
@@ -136,25 +109,10 @@
 		gap: 1rem;
 		color: var(--text);
 	}
-	button {
-		color: var(--text);
-	}
-	.accordion-trigger {
-		width: 100%;
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		gap: 1rem;
-	}
-	.accordion-buttons {
-		display: grid;
-		grid-template-columns: minmax(0, 1fr);
-		grid-auto-flow: row;
-		gap: 1rem;
-	}
+
 	.facts {
 		display: flex;
-		gap: 1rem;
+		flex-direction: column;
 	}
 	.right-col {
 		display: flex;
@@ -166,11 +124,34 @@
 		flex-direction: column;
 		gap: 1rem;
 		text-decoration: none;
-		border: 1px solid var(--border);
+		/* border: 1px solid var(--border); */
 		border-radius: 0.35rem;
 		padding: 1rem;
 	}
-	.product-card:visited {
-		border: 1px solid var(--border-muted);
+	/* .product-card:visited { */
+	/* 	border: 1px solid var(--border-muted); */
+	/* } */
+	.title {
+		background-color: var(--quinary);
+		padding: 0.5rem;
+		border-radius: 0.35rem;
+		color: var(--tertiary);
+	}
+	.title:visited {
+		background-color: var(--quinary-muted);
+	}
+	.notes {
+		padding-bottom: 2rem;
+	}
+	.metadata .footer {
+		display: grid;
+		grid-template-columns: minmax(0, 1fr) minmax(0, max-content);
+	}
+	.right-col > .footer {
+		display: flex;
+		justify-content: space-between;
+	}
+	.button-wrapper {
+		grid-column: 3/4;
 	}
 </style>
