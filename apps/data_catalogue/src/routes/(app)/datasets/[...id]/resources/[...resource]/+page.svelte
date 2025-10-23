38ef62c61e4791824248bed8358fc4be7b48baa7
artgp <git@arturoguzmanperez.com>
2025-10-23 14:49:02 +0100

    feat: add resource page

--- apps/data_catalogue/src/routes/(app)/datasets/[...id]/resources/[...resource]/+page.svelte
+++ apps/data_catalogue/src/routes/(app)/datasets/[...id]/resources/[...resource]/+page.svelte
@@ -1,32 +1,104 @@
 <script lang="ts">
 	import { debug } from '$lib/globals/dev.svelte'
+	import Notes from '$lib/ui/text/notes.svelte'
 	import { jstr } from '@arturoguzman/art-ui'
-	import { Button, Fact } from '@imago/ui'
+	import { BaseSection, Button, Subtitle, Paragraph, Icon, formatBytes, Title } from '@imago/ui'
+	import { DateTime } from 'luxon'
 
 	let { data } = $props()
 	let result = $derived(data.data.result)
 </script>
 
-<div class="page">
-	{#if Array.isArray(result) === false && typeof result.url === 'string'}
-		<Button href={result.url} download>Download: {result.name ?? result.description}</Button>
-	{/if}
+<BaseSection>
+	<div class="resource-section">
+		<div class="left-col">
+			<div class="file-metadata">
+				<div class="header">
+					{#if 'name' in result}
+						<Title size="md">{result.name}</Title>
+					{/if}
+					{#if 'description' in result}
+						<Notes note={String(result.description)}></Notes>
+					{/if}
+				</div>
+				<div class="metadata">
+					<Subtitle>Metadata</Subtitle>
+					{#if 'format' in result}
+						<div class="metadata-field">
+							<Subtitle>Format</Subtitle>
+							<Notes note={String(result.format)}></Notes>
+						</div>
+					{/if}
 
-	{#each Object.entries(result) as [key, value]}
-		{#if value !== null}
-			<Fact title={key} text={String(value)}></Fact>
-		{/if}
-	{/each}
-	{#if debug.status}
-		<pre>{jstr(data)}</pre>
-	{/if}
-</div>
+					{#if 'created' in result}
+						<div class="metadata-field">
+							<Subtitle>Created</Subtitle>
+							<Paragraph
+								>{DateTime.fromISO(String(result.created)).toLocaleString(
+									DateTime.DATETIME_FULL
+								)}</Paragraph
+							>
+						</div>
+					{/if}
+					{#if 'last_modified' in result}
+						<div class="metadata-field">
+							<Subtitle>Modified</Subtitle>
+							<Paragraph
+								>{DateTime.fromISO(String(result.last_modified)).toLocaleString(
+									DateTime.DATETIME_FULL
+								)}</Paragraph
+							>
+						</div>
+					{/if}
+					{#if 'size' in result}
+						<div class="metadata-field">
+							<Subtitle>Size</Subtitle>
+							<Paragraph>{formatBytes(Number(result.size))}</Paragraph>
+						</div>
+					{/if}
+				</div>
+			</div>
+		</div>
+		<div class="right-col">
+			{#if Array.isArray(result) === false && typeof result.url === 'string'}
+				<Button href={result.url} download
+					>Download: {result.name ?? result.description}
+					<Icon icon={{ icon: 'file-download', set: 'tabler' }}></Icon>
+				</Button>
+			{/if}
+		</div>
+	</div>
+</BaseSection>
+
+{#if debug.status}
+	<pre>{jstr(data)}</pre>
+{/if}
 
 <style>
-	.page {
-		color: var(--theme-colour-text);
+	.resource-section {
+		display: grid;
+		grid-template-columns: minmax(0, 1fr) minmax(0, 1fr);
+		min-height: 70lvh;
 	}
-	.page > * + * {
-		margin: 1rem;
+	.file-metadata {
+		display: flex;
+		flex-direction: column;
+		gap: 2rem;
+	}
+
+	.metadata {
+		width: 100%;
+		display: grid;
+		grid-template-columns: minmax(0, max-content) minmax(0, 1fr);
+		grid-auto-flow: row;
+		gap: 0rem 2rem;
+	}
+	.metadata-field {
+		display: grid;
+		grid-column: 1/-1;
+		grid-template-rows: subgrid;
+		grid-template-columns: subgrid;
+		width: 100%;
+		overflow-x: hidden;
 	}
 </style>
