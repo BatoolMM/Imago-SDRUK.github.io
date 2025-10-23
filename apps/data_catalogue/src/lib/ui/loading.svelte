320d8484055eb76a5ac7341b053c77891ee55679
artgp <git@arturoguzmanperez.com>
2025-10-23 14:49:44 +0100

    feat: add loading bar

---
+++ apps/data_catalogue/src/lib/ui/loading.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+	import { APP_STATE } from '$lib/globals/state.svelte'
+	import { slide } from 'svelte/transition'
+</script>
+
+{#if APP_STATE.loading}
+	<div
+		transition:slide
+		id="lightbar"
+		class={`lightbar ${APP_STATE.loading ? 'lightbar-loading' : ''}`}
+	></div>
+{/if}
+
+<style>
+	.lightbar {
+		height: 0.5rem;
+		width: 100lvw;
+		background-color: var(--background);
+		position: fixed;
+		top: 0;
+		left: 0;
+		transform: translateY(-100%);
+		transition:
+			transform 0.3s 0.3s ease-in-out,
+			border 0.3s ease-in-out;
+		z-index: 9999;
+	}
+
+	.lightbar-loading {
+		background: linear-gradient(
+			to right,
+			var(--background),
+			var(--background-accent),
+			var(--quinary),
+			var(--quinary-accent),
+			var(--quinary),
+			var(--background-accent),
+			var(--background)
+		);
+		/* background: linear-gradient( */
+		/* 	to right, */
+		/* 	var(--quinary-muted) 0%, */
+		/* 	var(--quinary) 10%, */
+		/* 	var(--quinary-accent) 20%, */
+		/* 	var(--secondary-muted) 30%, */
+		/* 	var(--secondary) 40%, */
+		/* 	var(--secondary-accent) 50%, */
+		/* 	var(--quinary-accent) 60%, */
+		/* 	var(--secondary-accent) 70%, */
+		/* 	var(--secondary-muted) 80%, */
+		/* 	var(--quinary) 90%, */
+		/* 	var(--quinary-muted) 100% */
+		/* ); */
+		background-size: 200% 100%;
+		animation: moveGradient 1s linear infinite;
+		transform: translateY(0%);
+	}
+
+	@keyframes moveGradient {
+		0% {
+			background-position: 100% center;
+		}
+
+		100% {
+			background-position: -100% center;
+		}
+	}
+</style>
--- apps/data_catalogue/src/routes/+layout.svelte
+++ apps/data_catalogue/src/routes/+layout.svelte
@@ -1,11 +1,24 @@
 <script lang="ts">
 	import '../app.css'
-	import favicon from '$lib/assets/favicon.svg';
-	let { children } = $props();
+	import favicon from '$lib/assets/favicon.svg'
+	import Loading from '$lib/ui/loading.svelte'
+	import { afterNavigate, beforeNavigate } from '$app/navigation'
+	import { APP_STATE } from '$lib/globals/state.svelte'
+	import { debug } from '$lib/globals/dev.svelte'
+	let { children } = $props()
+	beforeNavigate(() => {
+		APP_STATE.loading = true
+	})
+	afterNavigate(() => {
+		APP_STATE.loading = false
+	})
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
-
+{#if debug.status}
+	<pre>loading: {APP_STATE.loading}</pre>
+{/if}
 {@render children?.()}
+<Loading></Loading>
