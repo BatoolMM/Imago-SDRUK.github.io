6ddef672647fab64fa9a0965cd5019941692a6b3
artgp <git@arturoguzmanperez.com>
2025-11-10 12:21:52 +0000

    fix: set env as dynamic

--- apps/data_catalogue/src/routes/auth/[flow=auth_flows]/+page.server.ts
+++ apps/data_catalogue/src/routes/auth/[flow=auth_flows]/+page.server.ts
@@ -1,18 +1,18 @@
-import { IDENTITY_SERVER } from '$env/static/private'
 import type { PageServerLoadEvent } from './$types.js'
 import { redirect } from '@sveltejs/kit'
 import { handleOryResponse } from '$lib/utils/auth/index.js'
 import { DateTime } from 'luxon'
+import { env } from '$env/dynamic/private'
 
 export const load = async ({ cookies, url, params, fetch }: PageServerLoadEvent) => {
 	console.log('triggered', params.flow)
 	const id = url.searchParams.get('id')
 	const flow_id = url.searchParams.get('flow')
 	const return_to = url.searchParams.get('return_to')
-	let endpoint = `${IDENTITY_SERVER}/self-service/${params.flow}/flows?id=${flow_id}`
+	let endpoint = `${env.IDENTITY_SERVER}/self-service/${params.flow}/flows?id=${flow_id}`
 	if (params.flow === 'logout') {
 		console.log('logging out')
-		endpoint = `${IDENTITY_SERVER}/self-service/${params.flow}/browser`
+		endpoint = `${env.IDENTITY_SERVER}/self-service/${params.flow}/browser`
 		/**
 		 * NOTE: if there is no session here ory will redirect straight to flow error
 		 **/
@@ -29,11 +29,11 @@ export const load = async ({ cookies, url, params, fetch }: PageServerLoadEvent)
 	}
 	if (!flow_id && params.flow !== 'error') {
 		console.log('getting a new flow')
-		const endpoint = `${IDENTITY_SERVER}/self-service/${params.flow}/browser`
+		const endpoint = `${env.IDENTITY_SERVER}/self-service/${params.flow}/browser`
 		redirect(303, endpoint)
 	}
 	if (params.flow === 'error') {
-		endpoint = `${IDENTITY_SERVER}/self-service/errors?id=${id}`
+		endpoint = `${env.IDENTITY_SERVER}/self-service/errors?id=${id}`
 	}
 
 	const res = await fetch(endpoint)
@@ -70,7 +70,7 @@ export const load = async ({ cookies, url, params, fetch }: PageServerLoadEvent)
 		}
 		const redirect_to = data.error?.details?.redirect_to
 		if (redirect_to) {
-			const endpoint = `${IDENTITY_SERVER}/self-service/${params.flow}/browser`
+			const endpoint = `${env.IDENTITY_SERVER}/self-service/${params.flow}/browser`
 			redirect(307, endpoint)
 		}
 		redirect(307, `/`)
@@ -83,7 +83,7 @@ export const load = async ({ cookies, url, params, fetch }: PageServerLoadEvent)
 
 	if (expires_at.diffNow().milliseconds < 0) {
 		url.searchParams.delete('flow_id')
-		const endpoint = `${IDENTITY_SERVER}/self-service/${params.flow}/browser`
+		const endpoint = `${env.IDENTITY_SERVER}/self-service/${params.flow}/browser`
 		redirect(303, endpoint)
 	}
 	url.searchParams.delete('flow_id')
