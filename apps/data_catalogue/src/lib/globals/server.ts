0bb9b5847e205d1afa8335784313da656716345a
artgp <git@arturoguzmanperez.com>
2025-11-10 11:29:37 +0000

    feat: add server errors

--- apps/data_catalogue/src/lib/globals/server.ts
+++ apps/data_catalogue/src/lib/globals/server.ts
@@ -1,3 +1,27 @@
+export const SERVER_ERRORS: { [k: number]: [number, { message: string; id: string }] } = {
+	400: [
+		400,
+		{
+			message: `Please check your request and try again`,
+			id: 'bad-request'
+		}
+	],
+	403: [
+		403,
+		{
+			message: `You cannot do this.`,
+			id: 'forbidden'
+		}
+	],
+	404: [
+		404,
+		{
+			message: `We couldn't find this page or it may have been removed.`,
+			id: 'not-found'
+		}
+	]
+}
+
 export const COOKIES = {
 	access_token: 'imago-invite'
 }
