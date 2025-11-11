c28044b18cf86895b039bfab77dd136e2f1012da
artgp <git@arturoguzmanperez.com>
2025-11-11 23:10:39 +0000

    style: move title

--- apps/data_catalogue/src/lib/ui/products/product.svelte
+++ apps/data_catalogue/src/lib/ui/products/product.svelte
@@ -35,89 +35,117 @@
 
 <div class="product">
 	<div class="header">
-		<Title size="lg" text={String(result.title)}></Title>
+		<div class="product-title">
+			<Title size="lg" text={String(result.title)}></Title>
+		</div>
+
 		{#if 'notes' in result && result.notes !== null}
-			<Notes note={String(result.notes)}></Notes>
+			<div class="product-notes">
+				<Notes note={String(result.notes)}></Notes>
+			</div>
 		{/if}
 	</div>
-
-	{#if 'resources' in result && Array.isArray(result.resources) && result.resources.length > 0}
-		<div class="resources">
-			<Subtitle size="md" text="Resources"></Subtitle>
-			<div class="content">
-				<div class="pills">
-					{#each result.resources as resource}
-						<div class="pill">
-							<Subtitle size="xs">{resource.name ?? resource.description}</Subtitle>
-							<div class="pill-buttons">
-								<Button href="/datasets/{page.params.id}/resources/{resource.id}"
-									>{#snippet leftCol()}
-										<span>Details</span>
-									{/snippet}
-									{#snippet rightCol()}
-										<Icon icon={{ icon: 'arrow-narrow-right', set: 'tabler' }}></Icon>
-									{/snippet}
-								</Button>
-								{#if resource.format.toLowerCase() === 'html'}
-									<Button href={resource.url}
+	<div class="product-content">
+		{#if 'resources' in result && Array.isArray(result.resources) && result.resources.length > 0}
+			<div class="resources">
+				<Subtitle size="md" text="Resources"></Subtitle>
+				<div class="content">
+					<div class="pills">
+						{#each result.resources as resource}
+							<div class="pill">
+								<Subtitle size="xs">{resource.name ?? resource.description}</Subtitle>
+								<div class="pill-buttons">
+									<Button href="/datasets/{page.params.id}/resources/{resource.id}"
 										>{#snippet leftCol()}
-											<span>Visit</span>
+											<span>Details</span>
 										{/snippet}
 										{#snippet rightCol()}
-											<Icon icon={{ icon: 'arrow-up-right-01', set: 'hugeicons' }}></Icon>
+											<Icon icon={{ icon: 'arrow-narrow-right', set: 'tabler' }}></Icon>
 										{/snippet}
 									</Button>
-								{:else}
-									<Button href={resource.url} download
-										>{#snippet leftCol()}
-											<span>Download</span>
-										{/snippet}
-										{#snippet rightCol()}
-											<Icon icon={getFileFormatIcon(resource.format)}></Icon>
-										{/snippet}
-									</Button>
-								{/if}
+									{#if resource.format.toLowerCase() === 'html'}
+										<Button href={resource.url}
+											>{#snippet leftCol()}
+												<span>Visit</span>
+											{/snippet}
+											{#snippet rightCol()}
+												<Icon icon={{ icon: 'arrow-up-right-01', set: 'hugeicons' }}></Icon>
+											{/snippet}
+										</Button>
+									{:else}
+										<Button href={resource.url} download
+											>{#snippet leftCol()}
+												<span>Download</span>
+											{/snippet}
+											{#snippet rightCol()}
+												<Icon icon={getFileFormatIcon(resource.format)}></Icon>
+											{/snippet}
+										</Button>
+									{/if}
+								</div>
 							</div>
-						</div>
-					{/each}
+						{/each}
+					</div>
 				</div>
 			</div>
-		</div>
-	{/if}
-	<!-- {#if Array.isArray(result.extras) && result.extras.length > 0} -->
-	<!-- 	<div class="resources"> -->
-	<!-- 		<Subtitle size="md" text="Extras"></Subtitle> -->
-	<!-- 		<div class="content"> -->
-	<!-- 			<div class="pills"> -->
-	<!-- 				{#each result.extras as extra} -->
-	<!-- 					<div class="pill"> -->
-	<!-- 						<Subtitle size="xs">{extra.key}</Subtitle> -->
-	<!-- 						<div class="pill-buttons"> -->
-	<!-- 							<pre>{extra.value}</pre> -->
-	<!-- 						</div> -->
-	<!-- 					</div> -->
-	<!-- 				{/each} -->
-	<!-- 			</div> -->
-	<!-- 		</div> -->
-	<!-- 	</div> -->
-	<!-- {/if} -->
+		{/if}
+		<!-- {#if Array.isArray(result.extras) && result.extras.length > 0} -->
+		<!-- 	<div class="resources"> -->
+		<!-- 		<Subtitle size="md" text="Extras"></Subtitle> -->
+		<!-- 		<div class="content"> -->
+		<!-- 			<div class="pills"> -->
+		<!-- 				{#each result.extras as extra} -->
+		<!-- 					<div class="pill"> -->
+		<!-- 						<Subtitle size="xs">{extra.key}</Subtitle> -->
+		<!-- 						<div class="pill-buttons"> -->
+		<!-- 							<pre>{extra.value}</pre> -->
+		<!-- 						</div> -->
+		<!-- 					</div> -->
+		<!-- 				{/each} -->
+		<!-- 			</div> -->
+		<!-- 		</div> -->
+		<!-- 	</div> -->
+		<!-- {/if} -->
+	</div>
 </div>
 
 <style>
+	.product-title {
+		position: relative;
+		background-color: color-mix(in oklab, var(--background) 90%, transparent 10%);
+		padding: 1rem;
+		border: 1px solid var(--border);
+	}
+	.product-title::before {
+		content: '';
+		position: absolute;
+		bottom: -0.75rem;
+		left: 0.75rem;
+		height: 100%;
+		width: 100%;
+		background-color: var(--highlight);
+		z-index: -1;
+	}
+	.header {
+		display: flex;
+		flex-direction: column;
+		gap: 2rem;
+	}
 	.product {
 		display: flex;
 		flex-direction: column;
 		gap: 3rem;
-		padding: 2rem;
-		background-color: var(--background-muted);
 		border-radius: 0.35rem;
 	}
-
-	.header {
-		display: flex;
-		flex-direction: column;
-		gap: 1rem;
+	.product-notes {
+		background-color: var(--background-muted);
+		padding: 2rem;
+		border-radius: var(--radius);
 	}
+	.product-content {
+		background-color: var(--background-muted);
+	}
+
 	.resources {
 		display: flex;
 		flex-direction: column;
