06a7cf7e36bce94868a9fb5a91175ea0a8043bd1
artgp <git@arturoguzmanperez.com>
2025-12-06 13:44:03 +0000

    chore: refactor resources authorisation

--- apps/data_catalogue/src/routes/api/v1/resources/+server.ts
+++ apps/data_catalogue/src/routes/api/v1/resources/+server.ts
@@ -1,5 +1,7 @@
 import { env } from '$env/dynamic/private'
 import { SERVER_ERRORS } from '$lib/globals/server.js'
+import { authorise, ketoWrite } from '$lib/utils/auth/index.js'
+import { getResourceBasePermissions } from '$lib/utils/auth/permissions/index.js'
 import { create } from '$lib/utils/ckan/ckan.js'
 import {
 	cleanResourceURL,
@@ -9,17 +11,18 @@ import {
 } from '$lib/utils/files/azure/index.js'
 import { error, json } from '@sveltejs/kit'
 
-export const GET = async ({ request }) => {
-	const action = request.headers.get('action')
-	if (action === 'download') {
-		// return presigned url of asset, validate user request is authenticated
-		return json({ message: 'ok' })
-	} // return list of resources, validate user request is authenticated
-	return json({ message: 'ok' })
-}
-
 const POST_ACTIONS = ['signed_url', 'create']
+
 export const POST = async ({ request, url, params, locals }) => {
+	await authorise({
+		namespace: 'Endpoint',
+		session: locals.session,
+		relation: 'POST',
+		object: '/api/v1/resources'
+	})
+	if (!locals.session) {
+		error(...SERVER_ERRORS[401])
+	}
 	const action = url.searchParams.get('action')
 	if (!action || !POST_ACTIONS.includes(action)) {
 		return error(...SERVER_ERRORS[403])
@@ -60,85 +63,18 @@ export const POST = async ({ request, url, params, locals }) => {
 					size: data.size
 				})
 			)
+			if (!resource.success) {
+				error(...SERVER_ERRORS[500])
+			}
+			await ketoWrite.patchRelationships({
+				//@ts-expect-error ok ok ok
+				relationshipPatch: getResourceBasePermissions({
+					object: resource_id.id,
+					dataset_id: data.id
+				}).map((relation) => ({ action: 'insert', relation_tuple: relation }))
+			})
 			return json(resource.result)
 		}
 	}
 	return json({ message: 'ok' })
 }
-
-// {
-//     "cache_last_updated": null,
-//     "cache_url": null,
-//     "created": "2025-11-06T19:19:27.761062",
-//     "datastore_active": false,
-//     "description": null,
-//     "format": "",
-//     "hash": "",
-//     "id": "fb867ecf-77a3-4f54-9ae3-cfe9ae671d9b",
-//     "last_modified": null,
-//     "metadata_modified": "2025-11-06T19:19:27.754204",
-//     "mimetype": null,
-//     "mimetype_inner": null,
-//     "name": null,
-//     "package_id": "5f62cb59-73a8-4520-8aa0-5c14cdf8e759",
-//     "position": 11,
-//     "resource_type": null,
-//     "size": null,
-//     "state": "active",
-//     "url": "https://stimagoappdevuksouth001.blob.core.windows.net/imago-app-dev-001/LJy16uZh_7bm5uVkt80bz",
-//     "url_type": null
-//   }
-
-// import { Server } from '@tus/server'
-// import { AzureStore } from '@tus/azure-store'
-// import { FileStore } from '@tus/file-store'
-//
-// import { env } from '$env/dynamic/private'
-//
-// const store = {
-// 	local: new FileStore({ directory: './files' }),
-// 	azure: new AzureStore({
-// 		account: env.STORAGE_AZURE_ACCOUNT_NAME,
-// 		accountKey: env.STORAGE_AZURE_ACCOUNT_KEY,
-// 		containerName: env.STORAGE_AZURE_CONTAINER
-// 	})
-// }
-//
-// const server = new Server({
-// 	path: '/api/v1/resources',
-// 	datastore: store.azure,
-// 	onIncomingRequest: async () => {
-// 		console.log('incoming...')
-// 	},
-// 	onUploadCreate: async (req, upload) => {
-// 		console.log('upload create...')
-// 		return upload
-// 	}
-// })
-//
-// export const GET = async ({ request }) => {
-// 	const stuff = await server.handleWeb(request)
-// 	return new Response(stuff.body, {
-// 		status: stuff.status,
-// 		headers: stuff.headers,
-// 		statusText: stuff.statusText
-// 	})
-// }
-// export const POST = async ({ request }) => {
-// 	const stuff = await server.handleWeb(request)
-// 	return new Response(stuff.body, {
-// 		status: stuff.status,
-// 		headers: stuff.headers,
-// 		statusText: stuff.statusText
-// 	})
-// }
-// export const PATCH = async ({ request }) => {
-// 	const stuff = await server.handleWeb(request)
-// 	return new Response(stuff.body)
-// }
-// export const DELETE = server.handleWeb
-// export const OPTIONS = server.handleWeb
-// export const HEAD = async ({ request }) => {
-// 	const stuff = await server.handleWeb(request)
-// 	return new Response(stuff.body)
-// }
--- apps/data_catalogue/src/routes/api/v1/resources/[...id]/+server.ts
+++ apps/data_catalogue/src/routes/api/v1/resources/[...id]/+server.ts
@@ -1,11 +1,20 @@
+import { authorise, ketoWrite } from '$lib/utils/auth/index.js'
 import { remove } from '$lib/utils/ckan/ckan.js'
 import {
 	deleteBlob,
 	getSignedDownloadUrl,
 	loadStorageClient
 } from '$lib/utils/files/azure/index.js'
+import { redirect } from '@sveltejs/kit'
 
-export const GET = async ({ params }) => {
+export const GET = async ({ params, locals }) => {
+	await authorise({
+		namespace: 'Endpoint',
+		relation: 'GET',
+		session: locals.session,
+		object: '/api/v1/resources',
+		action: () => redirect(307, '/auth/login')
+	})
 	const id = params.id
 	const client = loadStorageClient()
 	const signed_url = getSignedDownloadUrl({ ...client, filename: id })
@@ -17,11 +26,18 @@ export const GET = async ({ params }) => {
 }
 
 export const DELETE = async ({ params, locals }) => {
+	await authorise({
+		namespace: 'Endpoint',
+		relation: 'DELETE',
+		session: locals.session,
+		object: '/api/v1/resources'
+	})
 	await locals.ckan.request(remove('resource_delete', { id: params.id }))
 	const client = loadStorageClient()
 	await deleteBlob({
 		...client,
 		filename: params.id
 	})
+	await ketoWrite.deleteRelationships({ namespace: 'Resource', object: params.id })
 	return new Response('', { status: 201, statusText: 'continue' })
 }
