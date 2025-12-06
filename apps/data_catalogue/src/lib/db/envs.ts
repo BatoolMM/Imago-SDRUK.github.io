e604bdac0e24a9ae50ae6148abaed82371be3b6a
artgp <git@arturoguzmanperez.com>
2025-12-06 13:26:51 +0000

    feat: add drizzle

---
+++ apps/data_catalogue/src/lib/db/envs.ts
@@ -0,0 +1,23 @@
+import { type } from 'arktype'
+import 'dotenv/config'
+
+const Env = type({
+	NODE_ENV: 'string',
+	DB_HOST: 'string',
+	DB_USER: 'string',
+	DB_PASSWORD: 'string',
+	DB_NAME: 'string',
+	DB_PORT: 'string.integer.parse',
+	DB_SSL: 'string | boolean',
+	DATABASE_URL: 'string',
+	DB_MIGRATING: 'string | boolean',
+	DB_SEEDING: 'string | boolean'
+})
+
+const validated = Env(process.env)
+
+if (validated instanceof type.errors) {
+	throw Error(validated.summary)
+}
+
+export const env = validated
