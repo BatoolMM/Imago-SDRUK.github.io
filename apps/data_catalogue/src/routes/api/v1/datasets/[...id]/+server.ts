fc7ab31809723e1c8e8bbeb9b23b4a9da3f305a2
artgp <git@arturoguzmanperez.com>
2025-12-06 13:41:16 +0000

    feat: add datasets endpoint

--- apps/data_catalogue/src/routes/api/v1/datasets/[...id]/+server.ts
+++ apps/data_catalogue/src/routes/api/v1/datasets/[...id]/+server.ts
@@ -1,7 +1,18 @@
+import { ketoCheck } from '$lib/utils/auth/index.js'
 import { patch } from '$lib/utils/ckan/ckan'
 import { json } from '@sveltejs/kit'
+import { error } from 'console'
 
 export const PATCH = async ({ request, locals, params }) => {
+	const permissions = await ketoCheck.checkPermission({
+		namespace: 'Dataset',
+		object: params.id,
+		relation: 'edit',
+		subjectId: locals.session?.identity.id
+	})
+	if (!permissions.allowed) {
+		error(401, 'Unautorised')
+	}
 	const data = await request.json()
 	const dataset = await locals.ckan.request(
 		patch('package_patch', { id: params.id }, { extras: data.extras, id: params.id })
