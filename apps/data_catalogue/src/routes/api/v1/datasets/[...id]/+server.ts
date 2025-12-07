57482a18c426c73c10e586b9c27886492304048e
artgp <git@arturoguzmanperez.com>
2025-12-07 21:27:54 +0000

    fix: replace auth fn

--- apps/data_catalogue/src/routes/api/v1/datasets/[...id]/+server.ts
+++ apps/data_catalogue/src/routes/api/v1/datasets/[...id]/+server.ts
@@ -1,18 +1,14 @@
-import { ketoCheck } from '$lib/utils/auth/index.js'
+import { authorise, ketoCheck } from '$lib/utils/auth/index.js'
 import { patch } from '$lib/utils/ckan/ckan'
 import { json } from '@sveltejs/kit'
-import { error } from 'console'
 
 export const PATCH = async ({ request, locals, params }) => {
-	const permissions = await ketoCheck.checkPermission({
+	await authorise({
 		namespace: 'Dataset',
-		object: params.id,
-		relation: 'edit',
+		relation: 'PATCH',
+		object: '/api/v1/datasets',
 		subjectId: locals.session?.identity.id
 	})
-	if (!permissions.allowed) {
-		error(401, 'Unautorised')
-	}
 	const data = await request.json()
 	const dataset = await locals.ckan.request(
 		patch('package_patch', { id: params.id }, { extras: data.extras, id: params.id })
