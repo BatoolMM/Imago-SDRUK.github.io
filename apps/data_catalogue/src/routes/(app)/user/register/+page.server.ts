5c1a07ac9cbf9e3f6763440d2391e732bb631c89
artgp <git@arturoguzmanperez.com>
2025-12-07 20:50:06 +0000

    feat: add user selfregistration webhooks and users endpoint

--- apps/data_catalogue/src/routes/(app)/user/register/+page.server.ts
+++ apps/data_catalogue/src/routes/(app)/user/register/+page.server.ts
@@ -1,30 +1,21 @@
 import type { PageServerLoadEvent } from './$types.js'
 import { fail, redirect } from '@sveltejs/kit'
-import { users } from '$lib/db/schema/users.js'
 import { handleDBError } from '$lib/utils/db/index.js'
-import { eq } from 'drizzle-orm'
 import { db } from '$lib/db/index.js'
-import { validateInsert } from '$lib/db/validation/index.js'
 import { parseForm } from '$lib/utils/forms/index.js'
-import { checkPermission, ketoRead, ketoWrite } from '$lib/utils/auth/index.js'
-import { answers, questions } from '$lib/db/schema/questions.js'
-import { validateAnswersForm } from '$lib/db/validation/questions.js'
+import { authorise } from '$lib/utils/auth/index.js'
+import { questions } from '$lib/db/schema/questions.js'
+import { error } from '@sveltejs/kit'
+import { users } from '$lib/db/schema/users.js'
+import { eq } from 'drizzle-orm'
 
 export const load = async ({ locals }: PageServerLoadEvent) => {
-	// NOTE: list permissions
-	// if permissions are non existent then allow user to 'register'
-	if (!locals.session) {
-		redirect(307, '/auth/login')
-	}
-	const user = await db
-		.select()
-		.from(users)
-		.where(eq(users.id, locals.session?.identity.id))
-		.limit(1)
-		.catch(handleDBError('Error fetching user'))
-	if (user.length > 0) {
-		redirect(307, '/user/account')
-	}
+	await authorise({
+		session: locals.session,
+		namespace: 'Endpoint',
+		relation: 'GET',
+		object: '/api/v1/questions'
+	})
 	const records = await db
 		.select()
 		.from(questions)
@@ -36,102 +27,23 @@ export const load = async ({ locals }: PageServerLoadEvent) => {
 }
 
 export const actions = {
-	create: async ({ request, locals }) => {
+	create: async ({ request, locals, fetch }) => {
 		if (!locals.session) {
 			redirect(307, '/')
 		}
 		const form = parseForm(await request.formData())
-		const validated_answers = await Promise.all(
-			validateAnswersForm({
-				answers: form.answer,
-				user_id: locals.session.identity.id
-			})
-		)
-		if (
-			validated_answers.filter((answer) => {
-				if (answer.answer.success === false) {
-					return true
-				}
-				return false
-			}).length > 0
-		) {
-			const errors = validated_answers
-				.filter((answer) => !answer.answer.success)
-				.map((answer) => !answer.answer.success && { ...answer.answer.errors })
-			return fail(401, { errors })
+		const body = Object.entries(form).map(([key, value]) => ({ question: key, answer: value }))
+		const res = await fetch(`/api/v1/answers`, {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json'
+			},
+			body: JSON.stringify(body)
+		}).catch(() => error(500, { message: 'Error saving answers', id: 'err' }))
+		if (!res.ok) {
+			error(400, { message: 'Error saving your answers, please try again', id: 'err' })
 		}
-		const existing_user = await db
-			.select()
-			.from(users)
-			.where(eq(users.id, locals.session.identity.id))
-		if (existing_user.length > 0) {
-			await db
-				.insert(answers)
-				.values(
-					validated_answers
-						.map((answer) => (answer.answer.success ? answer.answer.data : null))
-						.filter((answer) => answer !== null)
-				)
-				.catch(handleDBError('Error saving the answers'))
-			return redirect(307, '/user/account')
-		}
-		const user_form = {
-			id: locals.session.identity.id,
-			email: locals.session.identity.traits.email,
-			name: `${locals.session.identity.traits.name.first} ${locals.session.identity.traits.name.last}`,
-			status: 'active'
-		}
-		const user = validateInsert(users, user_form)
-		if (!user.success) {
-			return fail(400, { errors: user.errors })
-		}
-
-		const permissions = await createUserPermissions(locals.session.identity.id)
-		if (permissions.success) {
-			await db.insert(users).values(user.data)
-			await db
-				.insert(answers)
-				.values(
-					validated_answers
-						.map((answer) => (answer.answer.success ? answer.answer.data : null))
-						.filter((answer) => answer !== null)
-				)
-				.catch(handleDBError('Error saving the answers'))
-			return redirect(307, '/user/account')
-		}
-		return fail(500, { message: 'falied' })
-	}
-}
-
-async function createUserPermissions(user_id: string) {
-	const user_relationship = {
-		namespace: 'User',
-		object: user_id,
-		relation: 'members',
-		subject_id: user_id
-	}
-
-	const group_relationship = {
-		namespace: 'Group',
-		object: 'public',
-		relation: 'users',
-		subject_id: user_id
-	}
-	//TODO: handle each permissions error
-	try {
-		await ketoWrite.createRelationship({ createRelationshipBody: group_relationship })
-		await ketoWrite.createRelationship({
-			createRelationshipBody: user_relationship
-		})
-		console.log(`User ${user_id} created successfully and added to public group`)
-		return {
-			success: true
-		}
-	} catch (error) {
-		console.error(`Failed to create user ${user_id}:`, error)
-		return {
-			success: false,
-			error: error
-		}
+		await db.update(users).set({ status: 'active' }).where(eq(users.id, locals.session.identity.id))
+		return redirect(307, '/')
 	}
 }
---
+++ apps/data_catalogue/src/routes/api/v1/users/+server.ts
@@ -0,0 +1,48 @@
+import { env } from '$env/dynamic/private'
+import { db } from '$lib/db/index.js'
+import { users } from '$lib/db/schema/users.js'
+import { validateInsert } from '$lib/db/validation/index.js'
+import { authorise, ketoWrite } from '$lib/utils/auth'
+import { getUserBasePermissions } from '$lib/utils/auth/permissions/index.js'
+import { handleDBError } from '$lib/utils/db/index.js'
+import { json } from '@sveltejs/kit'
+import { error } from '@sveltejs/kit'
+
+export const GET = async ({ locals, request }) => {
+	await authorise({
+		session: locals.session,
+		namespace: 'Endpoint',
+		object: '/api/v1/users',
+		relation: 'GET'
+	})
+	return json({ message: 'ok' })
+}
+
+export const POST = async ({ locals, request, cookies }) => {
+	const preauthorised = cookies.get('kratos-api') === env.ADMIN_TOKEN
+	if (!preauthorised) {
+		await authorise({
+			session: locals.session,
+			namespace: 'Endpoint',
+			object: '/api/v1/users',
+			relation: 'POST'
+		})
+	}
+	const body = await request.json()
+	const user_form = {
+		id: body.id,
+		status: 'preregister'
+	}
+	const user = validateInsert(users, user_form)
+	if (!user.success) {
+		error(400, { message: String(user.errors), id: 'err' })
+	}
+
+	await ketoWrite.patchRelationships({
+		relationshipPatch: getUserBasePermissions({
+			id: user.data.id
+		}).map((relation) => ({ action: 'insert', relation_tuple: relation }))
+	})
+	await db.insert(users).values(user.data).catch(handleDBError('Error creating user'))
+	return json({ message: 'ok' })
+}
