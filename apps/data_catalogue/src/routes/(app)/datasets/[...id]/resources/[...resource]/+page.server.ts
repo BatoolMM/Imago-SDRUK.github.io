809abbb08d081b2521b82acec0e3495291b22baf
artgp <git@arturoguzmanperez.com>
2025-12-08 11:42:03 +0000

    feat: return versions to client

--- apps/data_catalogue/src/routes/(app)/datasets/[...id]/resources/[...resource]/+page.server.ts
+++ apps/data_catalogue/src/routes/(app)/datasets/[...id]/resources/[...resource]/+page.server.ts
@@ -1,9 +1,13 @@
+import { db } from '$lib/db/index.js'
+import { resource_versions } from '$lib/db/schema/resources.js'
 import { SERVER_ERRORS } from '$lib/globals/server.js'
 import { authorise, checkPermission } from '$lib/utils/auth/index.js'
 import { get } from '$lib/utils/ckan/ckan.js'
+import { handleDBError } from '$lib/utils/db/index.js'
 import { getFields } from '@imago/ui'
 import { redirect } from '@sveltejs/kit'
 import { error } from '@sveltejs/kit'
+import { eq } from 'drizzle-orm'
 export const load = async ({ locals, params }) => {
 	await authorise({
 		namespace: 'Resource',
@@ -12,6 +16,11 @@ export const load = async ({ locals, params }) => {
 		session: locals.session,
 		action: () => redirect(307, '/auth/login')
 	})
+	const versions = await db
+		.select()
+		.from(resource_versions)
+		.where(eq(resource_versions.resource, params.resource))
+		.catch(handleDBError('There are no versions for this resource'))
 	const data = await locals.ckan.request(get('resource_show', { id: params.resource }))
 	if (Array.isArray(data.result) || !data.result || !data.success) {
 		return error(...SERVER_ERRORS[404])
@@ -32,5 +41,5 @@ export const load = async ({ locals, params }) => {
 		'created',
 		'url'
 	])
-	return { data: { ...data, result } }
+	return { data: { ...data, result }, versions }
 }
