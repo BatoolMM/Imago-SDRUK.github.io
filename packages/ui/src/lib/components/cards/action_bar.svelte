dd4572a5e89cb12edc46750d52efd4be58e7b60a
artgp <git@arturoguzmanperez.com>
2026-03-25 15:06:33 +0000

    feat: add action bar

---
+++ packages/ui/src/lib/components/cards/action_bar.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte'
+
+	let { left, centre, right }: { left?: Snippet; centre?: Snippet; right?: Snippet } = $props()
+</script>
+
+<div class="edit-buttons">
+	<div class="left-col">
+		{@render left?.()}
+	</div>
+	<div class="centre-col">
+		{@render centre?.()}
+	</div>
+	<div class="right-col">
+		{@render right?.()}
+	</div>
+</div>
+
+<style>
+	.edit-buttons {
+		display: grid;
+		grid-template-columns: repeat(3, minmax(0, 1fr));
+	}
+	.left-col,
+	.centre-col,
+	.right-col {
+		display: flex;
+		gap: 0.25rem;
+	}
+	.left-col {
+		align-items: center;
+		justify-content: flex-start;
+	}
+	.centre-col {
+		align-items: center;
+		justify-content: flex-start;
+	}
+	.right-col {
+		align-items: center;
+		justify-content: flex-end;
+	}
+</style>
--- packages/ui/src/lib/components/cards/index.ts
+++ packages/ui/src/lib/components/cards/index.ts
@@ -1,4 +1,5 @@
 export { default as BaseCard } from './base_card.svelte'
+export { default as ActionBar } from './action_bar.svelte'
 export { default as Logos } from './logos.svelte'
 export { default as Banner } from './banner.svelte'
 export { default as Notice } from './notice.svelte'
