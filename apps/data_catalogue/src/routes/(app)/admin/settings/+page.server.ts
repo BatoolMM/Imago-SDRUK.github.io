ff4902a113a747c0672083ec68b3d8deb622814f
artgp <git@arturoguzmanperez.com>
2026-06-25 13:47:30 +0100

    feat: redir to first available route

--- apps/data_catalogue/src/routes/(app)/admin/settings/+page.server.ts
+++ apps/data_catalogue/src/routes/(app)/admin/settings/+page.server.ts
@@ -1,5 +1,16 @@
 import { redirect } from '@sveltejs/kit'
 
-export const load = () => {
-	redirect(307, '/admin/settings/datasets')
+export const load = async ({ parent }) => {
+	const { routes } = await parent()
+	const settings = routes.find((x) => x.label === 'Settings')
+	if (!settings) {
+		redirect(307, '/')
+	}
+	if (settings.subpaths.length === 0) {
+		redirect(307, '/')
+	}
+	const first = settings.subpaths[0]
+	if (first.href) {
+		redirect(307, first.href)
+	}
 }
