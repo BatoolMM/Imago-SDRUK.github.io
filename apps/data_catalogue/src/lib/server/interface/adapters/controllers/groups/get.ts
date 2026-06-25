7bcdbd884e28c98d2a27b8fc171fd92865e7764f
artgp <git@arturoguzmanperez.com>
2026-06-25 13:36:38 +0100

    feat: pass permissions to controllers

--- apps/data_catalogue/src/lib/server/interface/adapters/controllers/groups/get.ts
+++ apps/data_catalogue/src/lib/server/interface/adapters/controllers/groups/get.ts
@@ -1,6 +1,5 @@
 import { err, ok } from '$lib/server/entities/errors'
 import {
-	groupGetPublicUseCase,
 	groupGetUseCase,
 	groupGetUsersUseCase,
 	// groupsGetPublicUseCase,
@@ -10,7 +9,7 @@ import { getGroupsRepositoryModule } from '$lib/server/modules/groups'
 import { getIdentityModule } from '$lib/server/modules/identity'
 import type { IGroupsRepository } from '$lib/server/application/repositories/groups'
 import type { Configuration } from '$lib/server/entities/models/configuration'
-import { getServerContext } from '$lib/server/application/context'
+import { getServerContext, type AppContext } from '$lib/server/application/context'
 import { log } from '$lib/utils/server/logger'
 
 const presenter = ({ group }: { group: IGroupsRepository }) => group
@@ -18,29 +17,24 @@ const presenter = ({ group }: { group: IGroupsRepository }) => group
 export const groupGetController = async ({
 	session,
 	id,
-	configuration
+	configuration,
+	permissions
 }: {
 	session: App.Locals['session']
 	id?: string
 	configuration: Configuration
+	permissions?: AppContext['permissions']
 }) => {
 	if (!id) {
 		return err({ reason: 'Invalid Data', message: `You need to provide a group`, id: '' })
 	}
 	if (!session) {
-		const [errors, group] = await groupGetPublicUseCase({
-			id: id,
-			groups_repository: getGroupsRepositoryModule()
-		})
-		if (errors !== null) {
-			return err(errors)
-		}
-		return ok(group)
+		return err({ reason: 'Unauthorised' })
 	}
 	const [errors, group] = await groupGetUseCase({
 		id: id,
 		groups_repository: getGroupsRepositoryModule(),
-		...getServerContext({ session, configuration })
+		...getServerContext({ session, configuration, permissions })
 	})
 	if (errors !== null) {
 		log.error({ controller: 'groupGetController', errors })
