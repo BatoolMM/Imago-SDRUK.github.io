231aa7bb5fbf0c5ce47b65d43cc087ea5d6467d8
artgp <git@arturoguzmanperez.com>
2026-06-25 13:47:18 +0100

    feat: add downloads dashboard

--- apps/data_catalogue/src/routes/(app)/admin/dashboard/+page.server.ts
+++ apps/data_catalogue/src/routes/(app)/admin/dashboard/+page.server.ts
@@ -1,17 +1,18 @@
 import { downloadsGetAggregatesController } from '$lib/server/interface/adapters/controllers/downloads/get'
+import { permissionsCheckController } from '$lib/server/interface/adapters/controllers/permissions/get.js'
 import { error } from '@sveltejs/kit'
 
 export const load = async ({ locals, url }) => {
 	const from = url.searchParams.get('from')
 	const to = url.searchParams.get('to')
-	const [errors, downloads] = await downloadsGetAggregatesController({
+	const [downloads_errors, downloads] = await downloadsGetAggregatesController({
 		configuration: locals.configuration,
 		session: locals.session,
 		from,
 		to
 	})
-	if (errors !== null) {
-		return error(500, { message: errors.reason, id: errors.reason })
+	if (downloads_errors !== null) {
+		return error(500, { message: downloads_errors.reason, id: downloads_errors.reason })
 	}
 	return {
 		downloads
