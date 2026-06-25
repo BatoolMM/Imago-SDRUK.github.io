08528290bb3369673f405e925f64365746219c8b
artgp <git@arturoguzmanperez.com>
2026-06-25 13:30:06 +0100

    feat: allow passing custom checks

--- apps/data_catalogue/src/lib/server/application/context/index.ts
+++ apps/data_catalogue/src/lib/server/application/context/index.ts
@@ -2,6 +2,7 @@ import type { Transaction } from '$lib/db'
 import type { IAuthorisationService } from '$lib/server/application/services/autorisation'
 import type { Configuration } from '$lib/server/entities/models/configuration'
 import type { Session } from '$lib/server/entities/models/identity'
+import type { Permission } from '$lib/server/entities/models/permissions'
 import { getAuthorisationModule } from '$lib/server/modules/authorisation'
 
 export type AppContext = {
@@ -10,24 +11,28 @@ export type AppContext = {
 	authorisation_module: IAuthorisationService
 	identity_token?: string
 	tx?: Transaction
+	permissions?: Omit<Permission, 'actor'>[]
 }
 
 export const getServerContext = ({
 	session,
 	configuration,
 	identity_token,
-	tx
+	tx,
+	permissions
 }: {
 	session: Session
 	configuration: Configuration
 	identity_token?: string
 	tx?: Transaction
+	permissions?: Omit<Permission, 'actor'>[]
 }): AppContext => {
 	return {
 		session,
 		configuration,
 		authorisation_module: getAuthorisationModule(),
 		identity_token,
-		tx
+		tx,
+		permissions
 	}
 }
