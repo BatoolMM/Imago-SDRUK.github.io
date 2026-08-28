e61e4f8b45f70d098dc1d659f437cbd03fda2539
HaniaRiim19 <hania.riim19@gmail.com>
2026-08-28 16:23:14 +0100

    fix: preview for dataset adjusted to user selection on data catalogue

--- apps/data_catalogue/src/routes/(app)/datasets/+page.svelte
+++ apps/data_catalogue/src/routes/(app)/datasets/+page.svelte
@@ -95,7 +95,7 @@
 				{#if datasets.length === 0}
 					<Subtitle size="lg">There are no results for this search.</Subtitle>
 				{/if}
-				{#each datasets as dataset}
+				{#each datasets as dataset(dataset)}
 					<CardProduct {dataset}></CardProduct>
 				{/each}
 
